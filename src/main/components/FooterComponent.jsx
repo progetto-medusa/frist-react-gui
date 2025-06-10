e0f1de886e6dec6ec989d2e0a4844c3c7e81e73b
danb <accessoitaliano@gmail.com>
2025-06-10 09:42:09 +0200

    Graphical enhancement

--- src/main/components/FooterComponent.jsx
+++ src/main/components/FooterComponent.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/FooterComponent.css';
 export default function FooterComponent() {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} My App. Tutti i diritti riservati.</p>
+      <p>© {new Date().getFullYear()} Medusa's Lair. Tutti i diritti riservati...<br/><br/>0danb0</p>
     </footer>
   );
 }
